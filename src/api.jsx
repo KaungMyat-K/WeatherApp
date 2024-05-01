fd58cf5527106db0de710fb8a3eba2de1ebc74aa
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-05-01 23:17:07 +0630

    fetch data from weather api

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,6 @@
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'fb2b13bde586c191bb5f8649ae38b675';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
--- src/components/currentweather/currentweather.jsx
+++ src/components/currentweather/currentweather.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './currentweather.css'
 
 export default function currentweather({data}) {
   return (
